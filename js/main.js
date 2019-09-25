15bc394eac1ca95a6119660ca40653a8d048a786
benjamin83690 <55503103+benjamin83690@users.noreply.github.com>
2019-09-25 16:42:01 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+$(function(){
+
+    $(".navbar a, footer a").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 1000 , function(){window.location.hash = hash;})
+        
+    });
+
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                 
+                if(json.isSuccess) 
+                {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
+                    $('#contact-form')[0].reset();
+                }
+                else
+                {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#message + .comments').html(json.messageError);
+                }                
+            }
+        });
+    });
+
+    const sr = ScrollReveal();
+
+    sr.reveal('.navbar',{
+    	origin: 'left',
+    	distance: '100px',
+    	duration: 2000,
+    	// scale: 0.4,
+    	// rotate: {x:180, y:45, z:20},
+    	easing:'ease',
+    	// reset: true
+    });
+
+    sr.reveal('.rounded-circle',{
+    	origin: 'top',
+    	distance: '100px',
+    	duration: 2000,
+    	scale: 0.4,
+    	// rotate: {x:180, y:45, z:20},
+    	easing:'ease',
+    	// reset: true
+    });
+
+    sr.reveal('.heading',{
+    	duration: 1500,
+    	easing:'linear',
+    	delay: 300,
+    	reset: true
+    });
+
+    sr.reveal('.progress',{
+    	duration: 1000,
+    	easing:'linear',
+    	delay: 500,
+    	reset: true
+    });
+
+    sr.reveal('.timeline-panel',{
+    	origin: 'left',
+    	distance: '80px',
+    	duration: 1000,
+    	scale: 0.4,
+    	rotate: {x:100, y:45, z:10},
+    	easing:'linear',
+    	delay: 500,
+    	reset: true
+    }, 50);
+
+    sr.reveal('.projet',{
+    	duration: 2000,
+    	easing:'linear',
+    	reset: true
+    }, 500);
+
+    sr.reveal('.educ',{
+    	duration: 1000,
+    	easing:'linear',
+    	delay: 500,
+    	reset: true
+    });
+
+    sr.reveal('.form-box',{
+        duration: 1000,
+        easing:'linear',
+        delay: 500,
+        reset: true
+    });
+
+})
+
+$(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+
